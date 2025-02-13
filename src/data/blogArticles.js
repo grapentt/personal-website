6da0957cbdfc5df98f3ef32948cb63a051bda591
grapentt <grapentthomas@gmail.com>
2025-02-13 20:20:10 +0100

    new date

--- src/data/blogArticles.js
+++ src/data/blogArticles.js
@@ -3,9 +3,9 @@ import firstArticleLatex from './latexArticles/1Quaternions';
 const data = [
   {
     title: 'Quaternions and more: A first glimpse into the fourth dimenion',
-    subtitle: '2015 BVP Hackathon',
+    subtitle: 'Subtitle',
     image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    date: '2025-02-13',
     desc:
       'Very interesting and engaging article. Read more',
     content: firstArticleLatex,
