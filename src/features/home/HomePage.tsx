53251fb7a34496a07d1ec82de8d0e11f83a4b2c3
grapentt <grapentthomas@gmail.com>
2026-02-04 11:02:57 +0100

    :memo: Update Blog description

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -50,11 +50,71 @@ const Index: React.FC = () => (
           <br /><br />
           And along the way, we’ll occasionally dive into physics as an application (reverting the usual order).
           <br /><br />
-          If you’ve ever been curious about the structures that shape our understanding of space, symmetry, and higher dimensions,  
-          I invite you to join me on this journey.  
+          If you've ever been curious about the structures that shape our understanding of space, symmetry, and higher dimensions,
+          I invite you to join me on this journey.
           <br /><br />
-          <strong>Let’s explore the fascinating world of mathematics together.</strong>
+          <strong>Let's explore the fascinating world of mathematics together.</strong>
         </p>
+
+        <h3>Topics I Plan to Cover</h3>
+
+        <h4>Foundational Mathematics</h4>
+        <ul>
+          <li>Glimpse into 4d: Isoclinic rotations, quaternions and the hopf fibration</li>
+          <li>Linear algebra</li>
+          <li>Multilinear algebra and Tensors</li>
+          <li>Group theory</li>
+          <li>Representation Theory</li>
+          <li>Exterior algebra</li>
+          <li>Clifford/Geometric Algebra</li>
+        </ul>
+
+        <h4>Differential Geometry & Topology</h4>
+        <ul>
+          <li>The atoms of space: Cell and Handle decompositions</li>
+          <li>Topolofical Manifolds</li>
+          <li>The metric tensor: Riemannian Geometry</li>
+          <li>Lie groups and Lie algebras</li>
+          <li>My favourite manifolds: Projective spaces, Lie groups and Grassmannians</li>
+          <li>Homology</li>
+          <li>Cohomology and poincare duality</li>
+          <li>Differential forms and hodge duality</li>
+          <li>Intersection forms of 4-manifolds</li>
+          <li>Bundles</li>
+          <li>Connections, curvature, and holonomy</li>
+          <li>Characteristic classes</li>
+          <li>Morse Theory</li>
+          <li>h-cobordism theorem</li>
+          <li>Self-duality and Donaldson Theory</li>
+          <li>Spin groups, Spin structures and Spinors</li>
+          <li>Dirac Operator on Spin-manifolds</li>
+          <li>Spin<sup>c</sup> structures and Seiberg-Witten Theory</li>
+        </ul>
+
+        <h4>Mathematical Physics</h4>
+        <ul>
+          <li>Pseudo-Riemannian spaces and applications to relativity</li>
+          <li>Gauge theory in physics: Maxwell's equations</li>
+          <li>Gauge theory in physics: Yang-Mills equations</li>
+          <li>Gauge theory in physics: Minimal Coupling and Matter Fields</li>
+          <li>Standard Model of particle physics</li>
+          <li>Topological Quantim Field Theory</li>
+          <li>Twistor theory</li>
+          <li>Topological quantum computing</li>
+        </ul>
+
+        <h4>Machine Learning Applications</h4>
+        <ul>
+          <li>Mathematical foundations of Machine learning</li>
+          <li>Topological Data Analysis</li>
+          <li>Topological Deep Learning</li>
+          <li>Geometric Deep Learning</li>
+          <li>Category Theory: A unifying framework?</li>
+          <li>Embedding Spaces</li>
+          <li>Representiation Learning</li>
+          <li>Geometric approaches to explainable AI</li>
+          <li>... This list will certainly expand with time ....</li>
+        </ul>
 
       </article>
     </div>
